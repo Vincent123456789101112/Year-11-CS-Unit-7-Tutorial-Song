622b068786df3605653301da532cb0dd5934c10a
Vincent123456789101112 <vincents2026@student.cis.edu.hk>
2024-05-31 08:03:34 +0800

    YAY

--- src/Song.java
+++ src/Song.java
@@ -1,4 +1,49 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Song {
 
-    private String
+    private String title;
+    private String artist;
+    private ArrayList<String> listeners;
+
+    public Song(String title, String artist) {
+
+        this.title = title;
+        this.artist = artist;
+        listeners = new ArrayList<>();
+
+    }
+
+    public String getTitle() {
+
+        return title;
+
+    }
+
+    public String getArtist() {
+
+        return artist;
+
+    }
+
+    public int howMany(ArrayList<String> newListeners) {
+
+        int newInt = 0;
+
+        for (String listener : newListeners) {
+
+            if (!listeners.contains(listener.toLowerCase())) {
+
+                listeners.add(listener.toLowerCase());
+
+                newInt++;
+
+            }
+
+        }
+
+        return newInt;
+
+    }
 }
